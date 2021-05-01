edc9936a8a4e8263bc2cfc201eb9b16e58fc5fe3
yuhuakukude <15577334054@163.com>
2021-05-02 02:26:16 +0800

    Imprve

--- dist/sdk.esm.js
+++ dist/sdk.esm.js
@@ -368,7 +368,7 @@ function Currency(decimals, symbol, name) {
  * The only instance of the base class `Currency`.
  */
 
-Currency.ETHER = /*#__PURE__*/new Currency(18, 'HT', 'Ether');
+Currency.ETHER = /*#__PURE__*/new Currency(18, 'OKT', 'OKther');
 var ETHER = Currency.ETHER;
 
 var _WETH;
